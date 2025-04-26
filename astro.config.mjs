beaf3ce59125b8bf4cc88ec9c8d32390f4da1875
dungngminh <63831488+dungngminh@users.noreply.github.com>
2025-04-26 21:45:22 +0700

    fix path

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://komkatstudio.github.io/mathi_portal",
+  site: "https://trymathi.xyz",
   integrations: [mdx(), sitemap(), icon()],
 
   vite: {
