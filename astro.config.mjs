f28defd351028982ad79f28e5730cbd1eadeb405
dungngminh <63831488+dungngminh@users.noreply.github.com>
2025-04-26 21:25:03 +0700

    add config vercel for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://komkatstudio.github.io/mathi_portal",
   integrations: [mdx(), sitemap(), icon()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+  output: 'server',
+  adapter: vercel(),
 });
